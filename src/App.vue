dde3061e19175d45deee29bea52c4d81374f15c5
dmarcosc <daniel.marcos.cuesta@capgemini.com>
2020-12-05 23:57:59 +0100

    home page e imagenes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+    <HomePage />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HomePage,
   },
 };
 </script>
 
 <style>
-#app {
+body {
+  background:linear-gradient(to bottom, black , #020024);
+  height:100%;
+  background-attachment: fixed;
+}
+main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* background:url('./assets/images/calleh.jpg'); */
+  background:url('./assets/images/murocola.jpg'),url('./assets/images/pizzeria.jpg'),
+  url('./assets/images/muroHojas.jpg');
+  background-position:0px 0px, 600px 0px,1300px 0px;
+  background-repeat: no-repeat;
+  background-size: contain;
+  min-height: 1000px;
+  margin:0 auto;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <p>
+      <img src="../assets/images/logo.jpg" width="100" class="logo">
+      <img src="../assets/images/logorojo.jpg" width="100">
+      <img src="../assets/images/logo2.jpg" width="100" class="logo">
+      <img src="../assets/images/logo3.jpg" width="100" class="logo">
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.logo{
+  border-radius: 50px;
+}
+</style>
