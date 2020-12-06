2efed9b002af5dd2808bc8e18f1a8a25f3c771f8
dmarcosc <carcharoth92@gmail.com>
2020-12-07 00:59:24 +0100

    few changes

--- src/App.vue
+++ src/App.vue
@@ -20,16 +20,22 @@ export default {
 <style>
 body {
   background:linear-gradient(to bottom, black , #020024);
-  height:100%;
-  background-attachment: fixed;
+  min-height: 100%;
+  width: 100%;
+  position:absolute;
+  /* background-attachment: fixed; */
+  margin: 0;
 }
 main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 100%;
+  position:absolute;
   /* background:url('./assets/images/calleh.jpg'); */
-  background:url('./assets/images/murocola.jpg'),url('./assets/images/pizzeria.jpg'),
+  /* background:url('./assets/images/murocola.jpg'),url('./assets/images/pizzeria.jpg'),
   url('./assets/images/muroHojas.jpg');
-  background-position:0px 0px, 600px 0px,1300px 0px;
-  background-repeat: no-repeat;
+  background-position:0px 0px, 600px 0px,1300px 0px; */
+  background:url('./assets/images/tablones5.jpg');
+  /* background-repeat: no-repeat; */
   background-size: contain;
   min-height: 1000px;
   margin:0 auto;
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="home">
-    <p>
-      <img src="../assets/images/logo.jpg" width="100" class="logo">
-      <img src="../assets/images/logorojo.jpg" width="100">
-      <img src="../assets/images/logo2.jpg" width="100" class="logo">
-      <img src="../assets/images/logo3.jpg" width="100" class="logo">
-    </p>
+    <nav class="nav-bar">
+      <label class="nav-bar-label">LOCATION</label>
+      <label class="nav-bar-label">FOOD</label>
+      <div class="nav-div-logo">
+      <img src="../assets/images/logo2.jpg" class="logo">
+      </div>
+      <label class="nav-bar-label">ABOUT</label>
+      <label class="nav-bar-label">CONTACT</label>
+      <div class="div-social-icons">
+        <img src="../assets/images/ig.svg" class="nav-bar-icon">
+        <img src="../assets/images/fb.png" class="nav-bar-icon">
+      </div>
+    </nav>
+    <hr>
+    <div class="img-body-entrance">
+      <img src="../assets/images/calleh.jpg" style="height:450px;">
+    </div>
   </div>
 </template>
 
@@ -20,7 +31,43 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.home{
+  height:900px;
+}
+
+/*****NAV-BAR*****/
 .logo{
-  border-radius: 50px;
+  border-radius: 110px;
+  width:170px;
+  margin: 0 10px;
+}
+.nav-bar{
+display:flex;
+align-items: center;
+justify-content: center;
+height:170px;
+padding:10px;
+color:wheat;
+font-weight: 600;
+font-size:26px;
+}
+.nav-bar-label{
+margin: 15px;
+}
+.nav-bar-icon{
+  margin: 0 5px;
+  width:40px;
+}
+.div-social-icons{
+  width: 100%;
+  margin-right: 3em;
+  text-align: right;
+  position: absolute;
+}
+
+/******body********/
+.img-body-entrance{
+display: flex;
+justify-content: center;
 }
 </style>
